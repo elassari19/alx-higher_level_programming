cf03439c1c4eae53511992fc262a35043eb151d1
Hicham El Assari <elassari19@gmail.com>
2023-08-11 14:12:24 +0100

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     else:
         for i in range(1, count):
             add += int(argv.__getitem__(i))
-        print("{}".format(add)
+        print("{}".format(add))
