e957b871aa1babe9f8607fdb4372d60e3d8db0b5
Hicham El Assari <elassari19@gmail.com>
2023-08-11 13:58:41 +0100

    Create calculator_1.py

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+
+def add(a, b):
+    """addition func
+
+    Args:
+        a: first number
+        b: second number
+
+    Returns:
+        return addition result
+    """
+    return (a + b)
+
+
+def sub(a, b):
+    """subtraction func
+
+    Args:
+        a: first number
+        b: second integer
+
+    Returns:
+        return subtraction result
+    """
+    return (a - b)
+
+
+def mul(a, b):
+    """ multiplication func
+
+    Args:
+        a: first number
+        b: second number
+
+    Returns:
+        return multiplication result
+    """
+    return (a * b)
+
+
+def div(a, b):
+    """ division func
+
+    Args:
+        a: first number
+        b: second number
+
+    Returns:
+        return division result
+    """
+    return int(a / b)
