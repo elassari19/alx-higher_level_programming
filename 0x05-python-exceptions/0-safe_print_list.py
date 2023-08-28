e61544254d92e48bc8d4004bbacd47af59bf8989
elassari19 <elassari19@gmail.com>
2023-08-28 18:24:11 +0100

    0x05-python-exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,12 +2,12 @@
 
 def safe_print_list(my_list=[], x=0):
     i = 0
-    res = 0
+    response = 0
     for i in range(0, x):
         try:
             print("{}".format(my_list[i]), end="")
-            res += 1
+            response += 1
         except:
             continue
     print()
-    return res
+    return response
