c3ae236d5189037d6e9bccb08c6118648b36723d
elassari19 <elassari19@gmail.com>
2023-08-30 00:55:16 +0100

    0x06-python-classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
 """defines class"""
+
 class Square:
-    """
-    class propertie
-    """
+    """class propertie"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-
 """defines class"""
+
 class Square:
-    """
-    Class properties:
-    self, size
-    """
+    """Class properties"""
     def __init__(self, size):
-        """
-        instance
-        Args: self, size
+        """constructor
+        
+        Args: 
+            size: size of square lines
         """
         self.__size = size
