36d127f867ebb767dea65cc41e94f87dcda8a07c
elassari19 <elassari19@gmail.com>
2023-08-31 00:03:01 +0100

    0x06-python-classes

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""defines class"""
+
+
+class Square:
+    """
+    properties
+
+    Attributes:
+        size: size
+    """
+    def __init__(self, size=0):
+        """instances
+
+        Args:
+            size: size
+        """
+        self.__size = size
+
+    def area(self):
+        """square size
+
+        Returns: area size
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Returns: area size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter size.
+
+        Args:
+            value: size
+
+        Raises:
+            TypeError: number
+            ValueError: number
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        """prints"""
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            print("#" * (self.__size))
