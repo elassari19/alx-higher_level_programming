46acdaa118a6351ca4685fcda91cdf615e7e8ab5
elassari19 <elassari19@gmail.com>
2023-09-19 00:39:01 +0100

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,169 +4,100 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """Rectangle
-    Attributes:
-        width: int
-        height: int
-        x: int
-        y: int
-    """
-
+    """  Rectangle """
     def __init__(self, width, height, x=0, y=0, id=None):
-        """ construnctor
-        Args:
-            width: int
-            height: int
-            x: int
-            y: int
-            id: int
-        """
+        """  constructor """
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
-
-    def __str__(self):
-        """__str__"""
-        return ("[Rectangle] ({}) {:d}/{:d} - {:d}/{:d}". format(self.id, self.__x, self.__y, self.__width, self.__height))
 
     @property
     def width(self):
-        """width
-        Returns:
-            int: int
-        """
-        return self.__width
+        return (self.__width)
 
     @width.setter
-    def width(self, value):
-        """set width
-        Args:
-            value: int
-        Raises:
-            TypeError: is not integer
-            ValueError: is less than or equal to zero
-        """
-        if not isinstance(value, int):
+    def width(self, width):
+        if type(width) != int:
             raise TypeError("width must be an integer")
-        if value <= 0:
+        if width <= 0:
             raise ValueError("width must be > 0")
-        self.__width = value
+        self.__width = width
 
     @property
     def height(self):
-        """height
-        Returns:
-            int: int
-        """
-        return self.__height
+        return (self.__height)
 
     @height.setter
-    def height(self, value):
-        """set height
-        Args:
-            value: int
-        Raises:
-            TypeError: is not an integer
-            ValueError: is less than or equal to zero
-        """
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
-        self.__height = value
+    def height(self, height):
+        if type(height) != int:
+            raise TypeError("heigth must be an integer")
+        if height <= 0:
+            raise ValueError("heigth must be > 0")
+        self.__height = height
 
     @property
     def x(self):
-        """x
-        Returns:
-            int: int
-        """
-        return self.__x
+        return (self.__x)
 
     @x.setter
-    def x(self, value):
-        """set x
-        Args:
-            value: int
-        Raises:
-            TypeError: is not an integer
-            ValueError: is less than or equal to zero
-        """
-        if type(value) is not int:
+    def x(self, x):
+        if type(x) != int:
             raise TypeError("x must be an integer")
-        if value < 0:
+        if x < 0:
             raise ValueError("x must be >= 0")
-        self.__x = value
+        self.__x = x
 
     @property
     def y(self):
-        """y
-        Returns:
-            int: int
-        """
-        return self.__y
+        return (self.__y)
 
     @y.setter
-    def y(self, value):
-        """set y
-        Args:
-            value: int
-        Raises:
-            TypeError: is not an integer
-            ValueError: is less than or equal to zero
-        """
-        if type(value) is not int:
+    def y(self, y):
+        if type(y) != int:
             raise TypeError("y must be an integer")
-        if value < 0:
+        if y < 0:
             raise ValueError("y must be >= 0")
-        self.__y = value
+        self.__y = y
 
     def area(self):
-        """area
-        Returns:
-            int: int
-        """
-        return self.__width * self.__height
+        """  Returself """
+        return (self.__width * self.__height)
 
     def display(self):
-        """display"""
-        if self.__y > 0:
-            for i in range(self.__y):
-                print()
-            self.__y = 0
-        for i in range(self.__height):
-            for j in range(self.__width):
-                if self.__y == j:
-                    print(" " * self.__x, end="")
-                print("#", end="")
-            print()
+        """  display """
+        for i in range(self.__y):
+            print("")
+        for j in range(self.__height):
+            print(self.__x * " " + '#' * self.__width)
 
     def update(self, *args, **kwargs):
-        """update
+        """  update """
+        if args:
+            for arguments in range(len(args)):
+                if arguments == 0:
+                    self.id = args[arguments]
+                if arguments == 1:
+                    self.__width = args[arguments]
+                if arguments == 2:
+                    self.__height = args[arguments]
+                if arguments == 3:
+                    self.__x = args[arguments]
+                if arguments == 4:
+                    self.__y = args[arguments]
 
-        Args:
-            *args: arguments
-            **kwargs: kwargs
-        """
-        if args is not None and len(args) is not 0:
-            list_atrr = ['id', 'width', 'height', 'x', 'y']
-            for i in range(len(args)):
-                setattr(self, list_atrr[i], args[i])
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
+    def __str__(self):
+        """  __str__ """
+        return ("[{}] ({}) {:d}/{:d} - {:d}/{:d}".format(
+            self.__class__.__name__, self.id, self.__x,
+            self.__y, self.__width, self.__height))
+
     def to_dictionary(self):
-        """to_dictionary
-        Returns:
-            dict: dict
-        """
-        dict = {}
-        dict["id"] = self.id
-        dict["width"] = self.width
-        dict["height"] = self.height
-        dict["x"] = self.x
-        dict["y"] = self.y
-        return (dict)
+        """  to_dictionary """
+        return ({'id': self.id, 'x': self.x, 'height': self.height,
+                'width': self.width, 'y': self.y})
