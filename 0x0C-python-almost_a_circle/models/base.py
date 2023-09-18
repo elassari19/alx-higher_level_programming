997700e242ead2822f2943fa5f2470c705b8cfe0
elassari19 <elassari19@gmail.com>
2023-09-19 00:04:11 +0100

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-
+"""class"""
 
 from os import path
 import json
 
 
 class Base:
+"""Base"""
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-
+        """ """
         if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
@@ -19,6 +20,7 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """ """
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return '[]'
 
@@ -26,6 +28,7 @@ class Base:
 
     @classmethod
     def save_to_file(cls, list_objs):
+        """ """
         filename = cls.__name__ + '.json'
 
         with open(filename, mode='w', encoding='utf-8') as f:
@@ -41,6 +44,7 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
+        """ """
         if json_string is None or len(json_string) == 0:
             return []
 
@@ -48,6 +52,7 @@ class Base:
 
     @classmethod
     def create(cls, **dictionary):
+        """ """
         if cls.__name__ == 'Square':
             dummy = cls(3)
 
@@ -59,6 +64,7 @@ class Base:
 
     @classmethod
     def load_from_file(cls):
+        """ """
         filename = cls.__name__ + '.json'
 
         if path.exists(filename) is False:
