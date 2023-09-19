bc8f670d5d62ce05a88be80686d87ba60df97474
elassari19 <elassari19@gmail.com>
2023-09-19 01:00:08 +0100

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -3,93 +3,50 @@
 from models.rectangle import Rectangle
 
 
-from inspect import classify_class_attrs
-from models.rectangle import Rectangle
-
-
 class Square(Rectangle):
-    """Square
-    Attributes:
-        width: int
-        height: int
-        x: int
-        y: int
-        id: int
-    """
+    """ Square """
+
     def __init__(self, size, x=0, y=0, id=None):
-        """ constructor
-        Args:
-            size: int
-            x: int
-            y: int
-            id: int
-        """
+        """ constructor """
         super().__init__(size, size, x, y, id)
+        self.size = size
 
     def __str__(self):
-        """__str__"""
-        return ("[Square] ({}) {:d}/{:d} - {:d}".
-                format(self.id, self.x, self.y, self.size))
+        """ str """
+        return ("[{}] ({}) {:d}/{:d} - {:d}".format(
+            self.__class__.__name__, self.id, self.x,
+            self.y, self.size))
 
     @property
     def size(self):
-        """size
-        Returns:
-            int: size of one side of square.
-        """
-        return self.width
+        return (self.width)
 
     @size.setter
     def size(self, value):
-        """size
-        Args:
-            value: int
-        Raises:
-            TypeError: is not an integer.
-            ValueError: is less than or equal to zero.
-        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
-        
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
-        """update
+        """ update """
+        if args:
+            for argmts in range(len(args)):
+                if argmts == 0:
+                    self.id = args[argmts]
+                if argmts == 1:
+                    self.size = args[argmts]
+                if argmts == 2:
+                    self.x = args[argmts]
+                if argmts == 3:
+                    self.y = args[argmts]
 
-        Args:
-            *args: arguments
-            **kwargs: dictionary
-        """
-        if args is not None and len(args) is not 0:
-            list_atr = ['id', 'size', 'x', 'y']
-            for i in range(len(args)):
-                if list_atr[i] == 'size':
-                    setattr(self, 'width', args[i])
-                    setattr(self, 'height', args[i])
-                else:
-                    setattr(self, list_atr[i], args[i])
         else:
             for key, value in kwargs.items():
-                if key == 'size':
-                    setattr(self, 'width', value)
-                    setattr(self, 'height', value)
-                else:
-                    setattr(self, key, value)
+                setattr(self, key, value)
 
     def to_dictionary(self):
-        """ to_dictionary
-
-        Returns:
-            dict: dict
-        """
-        dict1 = self.__dict__
-        dict2 = {}
-        dict2['id'] = dict1['id']
-        dict2['size'] = dict1['_Rectangle__width']
-        dict2['x'] = dict1['_Rectangle__x']
-        dict2['y'] = dict1['_Rectangle__y']
-
-        return dict2
+        """ to_dictionary """
+        return ({'id': self.id, 'x': self.x, 'size': self.size, 'y': self.y})
