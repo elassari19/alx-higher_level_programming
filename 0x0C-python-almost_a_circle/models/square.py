58ea5c4858cbd7f87818c5f8021c224266f2a6c9
elassari19 <elassari19@gmail.com>
2023-09-20 20:10:45 +0100

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -40,52 +40,6 @@ class Square(Rectangle):
         """size
         Args:
             value: int
-        Raises:
-            TypeError: is not an integer.
-            ValueError: is less than or equal to zero.
-        """
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        
+        """        
         self.width = value
         self.height = value
-
-    def update(self, *args, **kwargs):
-        """update
-
-        Args:
-            *args: arguments
-            **kwargs: dictionary
-        """
-        if args is not None and len(args) is not 0:
-            list_atr = ['id', 'size', 'x', 'y']
-            for i in range(len(args)):
-                if list_atr[i] == 'size':
-                    setattr(self, 'width', args[i])
-                    setattr(self, 'height', args[i])
-                else:
-                    setattr(self, list_atr[i], args[i])
-        else:
-            for key, value in kwargs.items():
-                if key == 'size':
-                    setattr(self, 'width', value)
-                    setattr(self, 'height', value)
-                else:
-                    setattr(self, key, value)
-
-    def to_dictionary(self):
-        """ to_dictionary
-
-        Returns:
-            dict: dict
-        """
-        dict1 = self.__dict__
-        dict2 = {}
-        dict2['id'] = dict1['id']
-        dict2['size'] = dict1['_Rectangle__width']
-        dict2['x'] = dict1['_Rectangle__x']
-        dict2['y'] = dict1['_Rectangle__y']
-
-        return dict2
