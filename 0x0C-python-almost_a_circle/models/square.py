ba656e1beac98fc6adc7422cc8a4d6094f7268e6
elassari19 <elassari19@gmail.com>
2023-09-20 20:07:06 +0100

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """defines classe"""
 from models.rectangle import Rectangle
-from inspect import classify_class_attrs
-from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
