263a57a0bc3982e72ca924fa1379285385bd7d9f
elassari19 <elassari19@gmail.com>
2023-09-20 20:14:37 +0100

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,42 +4,47 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Square
-    Attributes:
-        width: int
-        height: int
-        x: int
-        y: int
-        id: int
-    """
+    """ Square """
+
     def __init__(self, size, x=0, y=0, id=None):
-        """ constructor
-        Args:
-            size: int
-            x: int
-            y: int
-            id: int
-        """
+        """ constructor """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """__str__"""
+        """ str """
         return '[{}] ({}) {}/{} - {}'.\
             format(type(self).__name__, self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
-        """size
-        Returns:
-            int: size of one side of square.
-        """
+        """size"""
         return self.width
 
     @size.setter
     def size(self, value):
-        """size
-        Args:
-            value: int
-        """        
+        """size setter"""
         self.width = value
         self.height = value
+
+    def __update(self, id=None, size=None, x=None, y=None):
+        """__updater"""
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        """update"""
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
+
+    def to_dictionary(self):
+        """to_dictionary"""
+        return {"id": self.id, "size": self.width,
+                "x": self.x, "y": self.y}
