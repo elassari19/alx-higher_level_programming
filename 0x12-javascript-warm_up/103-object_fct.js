5558540eee09b2f8229140b95903902b0d3f22bc
elassari19 <elassari19@gmail.com>
2023-11-13 18:04:37 +0100

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -10,18 +10,15 @@ const myObject = {
   type: 'object',
   value: 12
 };
-
 console.log(myObject);
 
-myObject.incr = () => {
-  return myObject.value++;
+myObject.incr = function () {
+  this.value++;
 };
 
 myObject.incr();
 console.log(myObject);
-
 myObject.incr();
 console.log(myObject);
-
 myObject.incr();
 console.log(myObject);
