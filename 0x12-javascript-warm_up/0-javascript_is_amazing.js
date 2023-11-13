4b40342645e69d5c27f5cb9fa46046b45e442321
elassari19 <elassari19@gmail.com>
2023-11-13 19:35:20 +0100

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,11 +1,4 @@
 #!/usr/bin/node
 
-/*
-  Write a script that prints “JavaScript is amazing”
-  You must use console.log(...) to print all output
-  You are not allowed to use var
-  sould print string “Javascript is amazing”
-*/
-
 const myVar = 'JavaScript is amazing';
 console.log(myVar);
