582282ea742cedb417de38cfc51780c1a2e478ee
elassari19 <elassari19@gmail.com>
2023-11-19 23:32:55 +0100

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,19 +7,25 @@ Your script should connect to a MySQL server running on localhost at port 3306
 Results must be sorted in ascending order by states.id
 Results must be displayed as they are in the example below
 Your code should not be executed when imported
-
 """
 
 import MySQLdb
 from sys import argv
 
+# The __name__
 if __name__ == "__main__":
+    # connect to MySQLdb
     mySQLdb = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], mySQLdb=argv[3], charset="utf8")
     currsor = mySQLdb.cursor()
+    #set query
     currsor.execute("SELECT * FROM states ORDER BY id ASC")
+    # get result
     rows = currsor.fetchall()
+    # print the result
     for row in rows:
         print(row)
+    # close the currsor
     currsor.close()
+    # colse MySQLdb connection
     mySQLdb.close()
