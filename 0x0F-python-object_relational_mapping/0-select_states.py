d43b504138d32742a0962c477e7c7b433550b501
elassari19 <elassari19@gmail.com>
2023-11-19 23:57:05 +0100

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,17 +8,16 @@ Results must be sorted in ascending order by states.id
 Results must be displayed as they are in the example below
 Your code should not be executed when imported
 """
-
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    mySQLdb = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                              passwd=argv[2], mySQLdb=argv[3], charset="utf8")
-    currsor = mySQLdb.cursor()
-    currsor.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = currsor.fetchall()
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
     for row in rows:
         print(row)
-    currsor.close()
-    mySQLdb.close()
+    cur.close()
+    db.close()
