3aec1470b773924de12469dbb4e3ee5d9da9b646
elassari19 <elassari19@gmail.com>
2023-11-20 00:16:26 +0100

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-should improve the files model_city.py and model_state.py, and save them as relationship_city.py and relationship_state.py
+should improve the files model_city.py and model_state.py,
+and save them as relationship_city.py and relationship_state.py
 """
 
 import sys
--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-sould deletes all State objects with a name containing the letter a from the database
+sould deletes all State objects with a name containing the letter a
 """
 import sys
 from sqlalchemy import create_engine
