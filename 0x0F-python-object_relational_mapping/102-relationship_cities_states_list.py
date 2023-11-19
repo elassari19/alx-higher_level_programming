50497f8b35fbeab6d9563bdc466b4d5c3e361cbc
elassari19 <elassari19@gmail.com>
2023-11-20 00:19:51 +0100

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -11,12 +11,12 @@ from relationship_state import Base, State
 from relationship_city import City
 
 if __name__ == "__main__":
-    sqlEngine = create_engine('mysqluser+mysqldb://{}:{}@localhost/{}'.format
-                              (sys.argv[1], sys.argv[2], sys.argv[3]),
-                              pool_pre_ping=True)
-    Base.metadata.create_all(sqlEngine)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
     Session = sessionmaker()
-    Session.configure(bind=sqlEngine)
+    Session.configure(bind=engine)
     session = Session()
     rows = session.query(State).all()
     for state in rows:
