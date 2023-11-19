d6ce5675366ff80a8a92984f19fc995231dc7eba
elassari19 <elassari19@gmail.com>
2023-11-19 23:17:31 +0100

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -10,7 +10,7 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    sqlEngine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+    sqlEngine = create_engine('mysqluser+mysqldb://{}:{}@localhost/{}'.format
                            (sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(sqlEngine)
--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -10,7 +10,7 @@ from model_state import Base, State
 from model_city import City
 
 if __name__ == "__main__":
-    sqlEngine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+    sqlEngine = create_engine('mysqluser+mysqldb://{}:{}@localhost/{}'.format
                            (sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(sqlEngine)
