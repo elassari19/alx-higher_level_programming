c6c4bf12f6792df79ac83226aa1db80a310b716b
elassari19 <elassari19@gmail.com>
2023-11-20 00:30:02 +0100

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -10,7 +10,7 @@ from model_state import Base, State
 from model_city import City
 
 if __name__ == "__main__":
-    engine = create_engine('mysqluser+mysqldb://{}:{}@localhost/{}'.format
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
                            (sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
